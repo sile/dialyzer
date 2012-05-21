r3m0t/otp

Remove needless conversion
The final version of the callgraph needs not be "reduced" for more efficient
copying.

lib/dialyzer/src/dialyzer_succ_typings.erl
@@ -309,7 +309,7 @@ compare_types_1([], [], _Strict, NotFixpoint) ->
 
 find_succ_typings(SCCs, #st{next_label = NextLabel, callgraph = Callgraph,
 			    plt = Plt} = State) ->
-  Init = {NextLabel, dialyzer_callgraph:mini_callgraph(Callgraph), Plt},
+  Init = {NextLabel, Callgraph, Plt},
   NotFixpoint =
     ?timing("typesig",
 	    dialyzer_coordinator:parallel_job(typesig, SCCs, Init)),

sile/dialyzer

src/dialyzer_succ_typings.erl
@@ -314,7 +314,7 @@ compare_types_1([], [], _Strict, NotFixpoint) ->
 
 find_succ_typings(SCCs, #st{codeserver = Codeserver, callgraph = Callgraph,
 			    plt = Plt} = State) ->
-  Init = {Codeserver, dialyzer_callgraph:mini_callgraph(Callgraph), Plt},
+  Init = {Codeserver, Callgraph, Plt},
   NotFixpoint =
     ?timing("typesig",
 	    dialyzer_coordinator:parallel_job(typesig, SCCs, Init)),